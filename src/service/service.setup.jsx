b979f3a18acdaa05ab9feff3a27e76307396994f
rakeshSingh <rakesh.singh@novoinvent.com>
2020-06-12 09:58:29 +0530

    base url updated

--- src/service/service.setup.jsx
+++ src/service/service.setup.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const BASE_URL = "http://3.7.41.59:9082";
+const BASE_URL = "http://3.7.41.59:9082/mydiginotes";
 
 const Post = (endPoint, data, header = null) => {
   const AUTH_TOKEN = Cookies.get("token");
   axios.defaults.headers.common["Authorization"] = AUTH_TOKEN;
-  return axios.post(`${endPoint}`, data, header);
+  return axios.post(`${BASE_URL}${endPoint}`, data, header);
 };
 const Get = (endPoint) => {
   const AUTH_TOKEN = Cookies.get("token");
