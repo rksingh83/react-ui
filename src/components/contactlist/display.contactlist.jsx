a0b0d5bae508a557eba9818c7832ea4cd47d2950
rakeshSingh <rakesh.singh@novoinvent.com>
2020-07-26 16:27:36 +0530

    cancel user

--- src/components/contactlist/display.contactlist.jsx
+++ src/components/contactlist/display.contactlist.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { ReactComponent as Share } from "../../assets/share.svg";
+
 import { Get, Post } from "../../service/service.setup";
 //import "./user-data.style.scss";
+import { ReactComponent as Close } from "../../assets/close.svg";
+import { ReactComponent as Share } from "../../assets/share.svg";
 const ContactList = ({ profileList, isShare, selected, hide, images }) => {
   //http://localhost:9082/shareFile
   console.log(images);
@@ -33,16 +35,32 @@ const ContactList = ({ profileList, isShare, selected, hide, images }) => {
       hide(false);
     } catch (error) {}
   }
+    async function removeContact(id) {
+      if (!window.confirm("Are You sure you want to remove ?")) return;
+  
+      try {
+
+
+        const contacts = await Post(`/cancelUserAddRequest`, {id});
+        if (contacts.data.code == "200") {
+          alert(contacts.data.message);
+        }
+        window.location.reload();
+      } catch (error) {}
+  }
   return (
     <div className="row">
       <div className="col">
         <h5>Your Contact List</h5>
         <ul className="list-group">
           {profileList.map((item, index) => (
-            <li className="list-group-item" key={index}>
+            <li className="list-group-item li-contact-list" key={index}>
               <span> {item.fullname}</span>
               <span style={{ paddingLeft: "50px" }}>
                 {isShare && <Share onClick={() => shareWith(item.id)}></Share>}
+              </span>
+              <span style={{ paddingLeft: "50px" }}>
+                {!isShare && <Close onClick={() => removeContact(item.id)}></Close>}
               </span>
             </li>
           ))}
