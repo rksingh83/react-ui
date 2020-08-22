5386667e1ff2b76c0ae2e06d8d7168125247fc45
rakeshSingh <rakesh.singh@novoinvent.com>
2020-08-22 11:34:48 +0530

    null

--- src/components/add-friend/display-group.jsx
+++ src/components/add-friend/display-group.jsx
@@ -39,8 +39,8 @@ const DisplayGroupList = ({
   };
   const getGroups = async (id) => {
     const list = await GetGroupMember(id);
-    if (list.data.data)
-      setMembersInGroup(list.data.data.userGroup[0].profileList);
+    console.log(list.data);
+    if (list.data.data && Object.keys(list.data.data)>0) setMembersInGroup(list.data.data.userGroup[0].profileList);
   };
   const saveGroupHandler = async (groupId) => {
     if (memberList.length > 0) {
