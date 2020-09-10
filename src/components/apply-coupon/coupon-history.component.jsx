bf4f6a93a67e0db1b448eab5f44dbe4f70e0567b
rakeshSingh <rakesh.singh@novoinvent.com>
2020-09-10 23:26:30 +0530

    table data

--- src/components/apply-coupon/coupon-history.component.jsx
+++ src/components/apply-coupon/coupon-history.component.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import DisplayTable from "./display-table.component";
+
+const getDataTime = (timeStamp) => {
+  let date = timeStamp.split("T");
+  return `${date[0]} ${date[1]}`;
+};
 const CouponHistory = ({ transactions, bookCounts }) => (
-  <div className ="row">
+  <div className="row">
     <div className="col-md-6">
-      <DisplayTable heads={["Coupon Name", "Date", "Count"]}>
-        {transactions.map((transaction ,index) => (
-          <tr key ={index}>
+      <h6>Coupon Transaction</h6>
+      <DisplayTable
+        heads={["Coupon Name", "Date", "Page Credited", "Page Balance"]}
+      >
+        {transactions.map((transaction, index) => (
+          <tr key={index}>
             <td>{transaction.token}</td>
-            <td>{transaction.createDateTime}</td>
+            <td>{getDataTime(transaction.createDateTime)}</td>
+
             <td>{transaction.page}</td>
+            <td>{transaction.totalPage}</td>
           </tr>
         ))}
       </DisplayTable>
     </div>
     <div className="col-md-6">
-      <DisplayTable heads={["Book Name", "Count"]}>
-      {bookCounts.map((book ,index) => (
-          <tr key ={`${book.bookName}_${index}`}>
+    <h6>Books Details</h6>
+      <DisplayTable heads={["Book Name", "Total Pages", "Latest Upload Date"]}>
+        {bookCounts.map((book, index) => (
+          <tr key={`${book.bookName}_${index}`}>
             <td>{book.bookName}</td>
             <td>{book.totalPage}</td>
-         
+            <td>{book.updatedDateTime}</td>
           </tr>
         ))}
       </DisplayTable>
