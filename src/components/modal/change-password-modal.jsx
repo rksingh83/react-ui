179bcdf1f08459ba2d5f723b7ad06d8879b58ec2
rakeshSingh <rakesh.singh@novoinvent.com>
2020-09-10 16:28:07 +0530

    loader updated

--- src/components/modal/change-password-modal.jsx
+++ src/components/modal/change-password-modal.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import CustomLoader from "../loader/loader";
 import TextField from "@material-ui/core/TextField";
 import { ReactComponent as Cross } from "../../assets/cross.svg";
 import { ChangePassword } from "../../service/sharefiles";
@@ -94,7 +95,7 @@ const inputHight  = {height:"1rem"}
   return (
     <Modal size="md" show={show} onHide={() => hide(false)} animation={true}>
       <Modal.Body>
-        <div className="item-center">{startLoader && <CircularProgress />}</div>
+        <div className="item-center">{startLoader && <CustomLoader />}</div>
         <Cross
           className="mb-2 ml-auto"
           variant="secondary"
