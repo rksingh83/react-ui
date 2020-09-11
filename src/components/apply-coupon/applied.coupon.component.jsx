72f35f3d5b1c4e442d8a573d56e87cbd31328450
rakeshSingh <rakesh.singh@novoinvent.com>
2020-09-11 11:32:42 +0530

    apply coupon moved into header

--- src/components/apply-coupon/applied.coupon.component.jsx
+++ src/components/apply-coupon/applied.coupon.component.jsx
@@ -1,26 +1,30 @@
 import React, { useState, useEffect } from "react";
 import CouponHistory from "./coupon-history.component";
-import ApplyCouponHeader from './apply-coupon.header';
-import SideBar from './sidebar.conponent'
+import ApplyCouponHeader from "./apply-coupon.header";
+import SideBar from "./sidebar.conponent";
 import { GetUserTransactions } from "../../service/common";
+import CustomLoader from "../loader/loader";
 const Coupon = ({ history }) => {
   const [bookCounts, setBookCounts] = useState([]);
   const [transactions, setTransactions] = useState([]);
+  const [startLoader, setStartLoader] = useState(false);
   useEffect(() => {
     getCouponHistory();
   }, []);
   const getCouponHistory = async () => {
+    setStartLoader(true)
     const response = await GetUserTransactions();
-    console.log(response.data);
     setTransactions(response.data.data.transactions);
     setBookCounts(response.data.data.booksCount);
+    setStartLoader(false)
   };
   return (
     <>
-      <ApplyCouponHeader/>
+      <ApplyCouponHeader startLoader ={setStartLoader} />
+      {startLoader && <CustomLoader />}
       <div className="row">
-        <SideBar/>
-        <div className="col-md-10">
+        <SideBar />
+        <div className="col-md-10 mt-4">
           <CouponHistory transactions={transactions} bookCounts={bookCounts} />
         </div>
       </div>
--- src/components/apply-coupon/apply-coupon.header.jsx
+++ src/components/apply-coupon/apply-coupon.header.jsx
@@ -1,28 +1,79 @@
-import React from "react";
-
-const ApplyCouponHeader = () => (
-  <div className="row">
-    <div className="col-md-12">
-      <nav className="navbar navbar-expand-lg navbar-light sec-header-bg">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className ="col-md-2"></div>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-           <ul className="navbar-nav  text-white">
-            <li className="nav-item">input button</li>
-          </ul>
-        </div>
-      </nav>
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { AddCoupon } from "../../service/common";
+const ApplyCouponHeader = ({ startLoader }) => {
+  const [coupon, setCoupon] = useState("");
+  const INPUT_FIELDS = { error: false, message: "" };
+  const [isError, setError] = useState(INPUT_FIELDS);
+  const saveCouponHandler = async () => {
+    if (coupon == "") {
+      setError({ error: true });
+      return;
+    }
+    startLoader(true);
+    const res = await AddCoupon(coupon);
+    if (res.data) {
+      alert(res.data.message);
+    }
+    setCoupon("");
+    startLoader(false);
+  };
+  const setCouponInput = (value) => {
+    setCoupon(value);
+    setError(INPUT_FIELDS);
+  };
+  return (
+    <div className="row">
+      <div className="col-md-12">
+        <nav className="navbar navbar-expand-lg navbar-light sec-header-bg">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="col-md-2"></div>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav  text-white">
+              <li className="nav-item">
+                <TextField
+                  value={coupon}
+                  onChange={(e) => setCouponInput(e.target.value)}
+                  id="outlined-basic"
+                  label="Enter Coupon"
+                  fullWidth
+                  name="currentPassword"
+                  variant="outlined"
+                  name="currentPassword"
+                  size="small"
+                  required
+                  error={isError.error}
+                  helperText={isError.message}
+                />
+              </li>
+              <li className="nav-item">
+                <Button
+                  onClick={saveCouponHandler}
+                  variant="contained"
+                  color="secondary"
+                  mt={3}
+                  className="ml-2"
+                >
+                  Apply Coupon
+                </Button>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
 export default ApplyCouponHeader;
