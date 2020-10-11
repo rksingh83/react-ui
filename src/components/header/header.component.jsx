6cc324ccfac1187bc5504da8d205433ff39a6e1b
rakeshSingh <rakesh.singh@novoinvent.com>
2020-10-11 12:13:11 +0530

    message position

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -64,84 +64,83 @@ const Header = ({
     setNotificationCount(currentCountState + 1);
   };
   return (
-    <nav className="navbar navbar-expand-lg navbar-light custom-bg">
+    <>
       {flashMessage && (
         <div className="flash-message">
-          <div className="message-container">
-            {flashMessage}
-            <span className="cross-btn">
-              <i onClick={() => setFashMessage(false)} class="fas fa-times"></i>
-            </span>
-          </div>
+          <span className="p-3">{flashMessage} </span>
+          <span className="cross-btn">
+            <i onClick={() => setFashMessage(false)} class="fas fa-times"></i>
+          </span>
         </div>
       )}
-      <Link className="logo-container navbar-brand" to="/">
-        <img style={imgStyle} src={require("../../assets/logo.png")}></img>
+      <nav className="navbar navbar-expand-lg navbar-light custom-bg">
+        <Link className="logo-container navbar-brand" to="/">
+          <img style={imgStyle} src={require("../../assets/logo.png")}></img>
 
-        <span className="home">My Digi Network</span>
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ml-auto text-white">
-          <li className="nav-item">
-            <Link className="option nav-link text-white" to="/">
-              Home
-            </Link>
-          </li>
+          <span className="home">My Digi Network</span>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto text-white">
+            <li className="nav-item">
+              <Link className="option nav-link text-white" to="/">
+                Home
+              </Link>
+            </li>
 
-          {currentUser && (
-            <li className="nav-item">
-              <Link
-                className="option nav-link text-white"
-                to="/add-friend/USERS"
-              >
-                Contacts
-              </Link>
-            </li>
-          )}
-          {currentUser ? (
-            ""
-          ) : (
-            <li className="nav-item">
-              <Link className="option nav-link text-white" to="/login">
-                Login
-              </Link>
-            </li>
-          )}
-          {currentUser ? (
-            ""
-          ) : (
-            <li className="nav-item">
-              <Link className="option nav-link text-white" to="/signup">
-                Signup
-              </Link>
-            </li>
-          )}
-          {currentUser && (
-            <li className="nav-item">
-              <Link className="option nav-link text-white" to="/profile">
-                Profile
-              </Link>
-            </li>
-          )}
-          {currentUser && (
-            <li className="nav-item">
-              <Link className="option nav-link text-white" to="/coupon">
-                Coupon
-              </Link>
-            </li>
-          )}
-          {/* <li className="nav-item">
+            {currentUser && (
+              <li className="nav-item">
+                <Link
+                  className="option nav-link text-white"
+                  to="/add-friend/USERS"
+                >
+                  Contacts
+                </Link>
+              </li>
+            )}
+            {currentUser ? (
+              ""
+            ) : (
+              <li className="nav-item">
+                <Link className="option nav-link text-white" to="/login">
+                  Login
+                </Link>
+              </li>
+            )}
+            {currentUser ? (
+              ""
+            ) : (
+              <li className="nav-item">
+                <Link className="option nav-link text-white" to="/signup">
+                  Signup
+                </Link>
+              </li>
+            )}
+            {currentUser && (
+              <li className="nav-item">
+                <Link className="option nav-link text-white" to="/profile">
+                  Profile
+                </Link>
+              </li>
+            )}
+            {currentUser && (
+              <li className="nav-item">
+                <Link className="option nav-link text-white" to="/coupon">
+                  Coupon
+                </Link>
+              </li>
+            )}
+            {/* <li className="nav-item">
             <a
               className="option nav-link text-white"
               href="./app-debug.apk"
@@ -150,38 +149,39 @@ const Header = ({
               Download Android APK
             </a>
           </li> */}
-          <li className="nav-item">
-            <Link className="option nav-link text-white" to="contact-us">
-              Help
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="option nav-link text-white" to="tnc">
-              Terms And Condition
-            </Link>
-          </li>
-          {currentUser && (
             <li className="nav-item">
-              <Link className="option nav-link text-white" to="/notification">
-                <i className="fas fa-bell " style={{ fontSize: "25px" }}></i>
-                <span className="badge badge-danger mb-2">
-                  {userNotificationCount}
-                </span>
+              <Link className="option nav-link text-white" to="contact-us">
+                Help
               </Link>
             </li>
-          )}
-          {currentUser ? (
             <li className="nav-item">
-              <Link className="option nav-link text-white" to="/logout">
-                <Logout style={{ height: "28px" }} />
+              <Link className="option nav-link text-white" to="tnc">
+                Terms And Condition
               </Link>
             </li>
-          ) : (
-            ""
-          )}
-        </ul>
-      </div>
-    </nav>
+            {currentUser && (
+              <li className="nav-item">
+                <Link className="option nav-link text-white" to="/notification">
+                  <i className="fas fa-bell " style={{ fontSize: "25px" }}></i>
+                  <span className="badge badge-danger mb-2">
+                    {userNotificationCount}
+                  </span>
+                </Link>
+              </li>
+            )}
+            {currentUser ? (
+              <li className="nav-item">
+                <Link className="option nav-link text-white" to="/logout">
+                  <Logout style={{ height: "28px" }} />
+                </Link>
+              </li>
+            ) : (
+              ""
+            )}
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 };
 const mapDispatchToProps = (dispatch) => ({
