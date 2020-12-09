0a76699ef34bdd2ad646d43793b16f07e77beef4
rakeshSingh <rakesh.singh@novoinvent.com>
2020-12-09 11:18:44 +0530

    send invation

--- src/service/common.js
+++ src/service/common.js
@@ -1,74 +1,83 @@
 import {
-  Post,
-  Get
+    Post,
+    Get
 } from './service.setup';
 
 
-const AddCoupon = async (token) => {
-  try {
-    const response = await Post("/validateBookCoupon", {
-      token
-    });
+const AddCoupon = async(token) => {
+    try {
+        const response = await Post("/validateBookCoupon", {
+            token
+        });
 
-    if (response.data.code == "203") {
-      alert(response.data.message);
-      return;
+        if (response.data.code == "203") {
+            alert(response.data.message);
+            return;
+        }
+        if (response.data.code == "401") {
+            alert(response.data.message);
+            return;
+        }
+        return response;
+    } catch (e) {
+        alert("Something went wrong try latter");
+        //history.goBack();
     }
-    if (response.data.code == "401") {
-      alert(response.data.message);
-      return;
+
+}
+const GetPageLimits = async() => {
+    try {
+        return await Get("getUserPageLimit");
+    } catch (e) {
+
     }
-    return response;
-  } catch (e) {
-    alert("Something went wrong try latter");
-    //history.goBack();
-  }
 
 }
-const GetPageLimits = async () => {
-  try {
-    return await Get("getUserPageLimit");
-  } catch (e) {
+const GetUserTransactions = async() => {
+    try {
+        return await Get("getUserTransactionList");
+    } catch (e) {
 
-  }
-
-}
-const GetUserTransactions = async () => {
-  try {
-    return await Get("getUserTransactionList");
-  } catch (e) {
-
-  }
+    }
 
 }
 
-const SendFeedBack = async (feedback, name, email, phone) => {
-  try {
-    const response = await Post("/contactUs", {
-      feedback,
-      name,
-      email,
-      phone
-    })
-    return response;
-  } catch (e) {
-    alert("Something went wrong try latter");
-    //history.goBack();
-  }
+const SendFeedBack = async(feedback, name, email, phone) => {
+    try {
+        const response = await Post("/contactUs", {
+            feedback,
+            name,
+            email,
+            phone
+        })
+        return response;
+    } catch (e) {
+        alert("Something went wrong try latter");
+        //history.goBack();
+    }
 }
-const GetUserDetails = async () => {
-  try {
-    return await Get("getUserProfileByToken");
-  } catch (e) {
+const GetUserDetails = async() => {
+    try {
+        return await Get("getUserProfileByToken");
+    } catch (e) {
 
-  }
+    }
+
+}
+const sendPageLookupInvitation = async(request) => {
+    try {
+        return await Post("/scriptAccount", request);
+    } catch (e) {
+
+    }
 
 }
 export {
-  AddCoupon,
-  GetPageLimits,
-  GetUserTransactions,
-  SendFeedBack ,
-  GetUserDetails
+    AddCoupon,
+    GetPageLimits,
+    GetUserTransactions,
+    SendFeedBack,
+    GetUserDetails,
+    sendPageLookupInvitation
 
 }
